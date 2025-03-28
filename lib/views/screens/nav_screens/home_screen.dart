ff33b8ef25e714e90ce676590e8acc7586c524ae
shshaheen <kolimishaheen9885@gmail.com>
2025-03-28 21:39:48 +0530

    Work On Home-Screen Front-end

--- lib/views/screens/nav_screens/home_screen.dart
+++ lib/views/screens/nav_screens/home_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:smart_cart/views/screens/nav_screens/widgets/header_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
+    return const Scaffold(      
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderWidget(),
+          ],
+        ),
       ),
-      body: Center(child: Text("Home Screen")),
     );
   }
 }
