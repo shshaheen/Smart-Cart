ad7bcd58848f9e802b5bb99cafda80870a822789
shshaheen <kolimishaheen9885@gmail.com>
2025-04-02 18:08:07 +0530

    Display Subcategories by Category

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smart_cart/views/screens/nav_screens/account_screen.dart';
 import 'package:smart_cart/views/screens/nav_screens/cart_screen.dart';
+import 'package:smart_cart/views/screens/nav_screens/category_screen.dart';
 import 'package:smart_cart/views/screens/nav_screens/favorite_screen.dart';
 import 'package:smart_cart/views/screens/nav_screens/home_screen.dart';
 import 'package:smart_cart/views/screens/nav_screens/stores_screen.dart';
@@ -16,6 +17,7 @@ class MainScreenState extends State<MainScreen> {
   final List<Widget> _pages = [
     HomeScreen(),
     FavoriteScreen(),
+    CategoryScreen(),
     StoresScreen(),
     CartScreen(),
     AccountScreen(),
@@ -28,7 +30,7 @@ class MainScreenState extends State<MainScreen> {
           unselectedItemColor: Colors.grey,
           currentIndex: _pageIndex,
           onTap: (value) {
-            print(value);
+            // print(value);
             setState(() {
               
               _pageIndex = value;
@@ -45,6 +47,9 @@ class MainScreenState extends State<MainScreen> {
             BottomNavigationBarItem(
                 icon: Image.asset('assets/icons/love.png', width: 25),
                 label: "Favorite"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category),
+                label: "Categories"),
             BottomNavigationBarItem(
                 icon: Image.asset('assets/icons/mart.png', width: 25),
                 label: "Stores"),
---
+++ lib/views/screens/nav_screens/category_screen.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smart_cart/controllers/category_controller.dart';
+import 'package:smart_cart/controllers/subcategory_controller.dart';
+import 'package:smart_cart/models/category.dart';
+import 'package:smart_cart/models/subcategory.dart';
+// import 'package:smart_cart/views/screens/nav_screens/widgets/category_item_widget.dart';
+import 'package:smart_cart/views/screens/nav_screens/widgets/header_widget.dart';
+
+class CategoryScreen extends StatefulWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  // A future that will hold the list of categories once loaded from the API
+
+  late Future<List<Category>> futureCategories;
+  Category? _selectedCategory;
+  List<Subcategory> _subcategories = [];
+  final SubcategoryController _subcategoryController = SubcategoryController();
+
+  @override
+  void initState() {
+    super.initState();
+    futureCategories = CategoryController().loadCateegories();
+    //once the categories are loaded process then
+    futureCategories.then((categories) {
+      // iterate through the categories to find the "Fashion" category
+      for (var category in categories) {
+        if (category.name == 'Fashion') {
+          // If  fashion category is found then set it as the selected category
+          setState(() {
+            _selectedCategory = category;
+          });
+          // load subcategories for the "Fashion" category
+          _loadsubcategory(category.name);
+        }
+      }
+      _selectedCategory = categories.first;
+      _loadsubcategory(_selectedCategory!.name);
+    });
+  }
+
+  //this will load subcategories based on the categoryName
+  Future<void> _loadsubcategory(String categoryName) async {
+    final subcategories = await _subcategoryController
+        .getSubcategoriesByCategoryName(categoryName);
+    setState(() {
+      _subcategories = subcategories;
+      print(subcategories);
+      print(categoryName);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 20),
+        child: SizedBox(height: 120, child: HeaderWidget()),
+      ),
+      body: Row(
+        children: [
+          //left-side - Display categories
+          Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.grey.shade200,
+                child: FutureBuilder(
+                    future: futureCategories,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(
+                          child: Text("Error: ${snapshot.error}"),
+                        );
+                      } else {
+                        final categories = snapshot.data!;
+                        return ListView.builder(
+                            // scrollDirection: Axis.horizontal,
+                            itemCount: categories.length,
+                            itemBuilder: (context, index) {
+                              final category = categories[index];
+                              return ListTile(
+                                onTap: () {
+                                  setState(() {
+                                    _selectedCategory = category;
+                                  });
+                                  _loadsubcategory(category.name);
+                                },
+                                title: Text(
+                                  category.name,
+                                  style: GoogleFonts.quicksand(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: category == _selectedCategory
+                                          ? Colors.blue
+                                          : Colors.black),
+                                ),
+                              );
+                            });
+                      }
+                    }),
+              )),
+          //right-side - Display selected category details
+          Expanded(
+            flex: 5,
+            child: _selectedCategory != null
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          _selectedCategory!.name,
+                          style: GoogleFonts.quicksand(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            letterSpacing: 1.7,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 150,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(_selectedCategory!.banner),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                      _subcategories.isNotEmpty
+                          ? GridView.builder(
+                              shrinkWrap: true,
+                              itemCount: _subcategories.length,
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3,
+                                      crossAxisSpacing: 8,
+                                      mainAxisSpacing: 4),
+                              itemBuilder: (context, index) {
+                                final subcategory = _subcategories[index];
+                                return Column(
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey.shade200),
+                                      child: Center(
+                                        child: Image.network(
+                                          subcategory.image,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        subcategory.subCategoryName,
+                                        style: GoogleFonts.quicksand(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                );
+                              })
+                          : Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(
+                                  child: Text(
+                                "No SubCategories",
+                                style: GoogleFonts.quicksand(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 1.7,
+                                ),
+                              )),
+                            ),
+                    ],
+                  )
+                : Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
