f2e0ea71a809dcc624a8af4478be051c4682d82c
shshaheen <kolimishaheen9885@gmail.com>
2025-04-10 18:28:03 +0530

    Backend get product by category by category API

--- backend_api/routes/product.js
+++ backend_api/routes/product.js
@@ -38,4 +38,19 @@ productRouter.get('/api/recommended-products', async (req, res) => {
         return res.status(500).json({error: e.message});
     }
 });
+
+// New route for retrieving products by category
+productRouter.get('/api/products-by-category/:category', async(req, res) =>{
+    try {
+        const {category} = req.params;
+        const products = await Product.find({category});
+        if(!products || products.length == 0){
+            return res.status(404).json({msg:"Product not found"});
+        }else{
+            return res.status(200).json(products)
+        }
+    } catch (e) {
+        res.status(500).json({error: e.message});
+    }
+})
 module.exports = productRouter;
